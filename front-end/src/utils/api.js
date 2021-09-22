c5d557c3648d8abcac31e08b5e2570b611af79d7
EMERIC237 <bestvemeric@gmail.com>
2021-09-22 19:12:14 -0400

    api

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,8 +5,9 @@
 // import formatReservationDate from "./format-reservation-date";
 // import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+// const API_BASE_URL =
+//   process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = "https://resto-reservation-backend.herokuapp.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
